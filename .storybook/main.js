2c832644eaf9aba3c51cf0475cb763acb94ab006
Lukasb38 <50137992+lukeb36@users.noreply.github.com>
2023-09-25 23:41:17 +0200

    Initialize caret-dropdown-with-buttons

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../ui/app/components/**/*.stories.js', '../ui/app/pages/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-actions',
+    '@storybook/addon-backgrounds'
+  ],
+}
