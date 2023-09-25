475e573096815c185ed099f6977cba50c1f0628e
Lukasb38 <50137992+lukeb36@users.noreply.github.com>
2023-09-25 23:53:26 +0200

    Initialize experiment-no-ui-libs-home

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  restoreMocks: true,
+  coverageDirectory: 'jest-coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 5.83,
+      functions: 8.28,
+      lines: 11.18,
+      statements: 11.21,
+    },
+  },
+  setupFiles: ['./test/setup.js', './test/env.js'],
+  testMatch: ['<rootDir>/ui/app/**/swaps/**/*.test.js'],
+};
