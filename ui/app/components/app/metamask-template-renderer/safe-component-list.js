b7f276b03dd08b42c9c549c61e9ad135b597b5dc
Lukasb38 <50137992+lukeb36@users.noreply.github.com>
2023-09-25 23:47:27 +0200

    Initialize lavamoat-buildsystem-auto-policy-changes

---
+++ ui/app/components/app/metamask-template-renderer/safe-component-list.js
@@ -0,0 +1,23 @@
+import Button from '../../ui/button';
+import Chip from '../../ui/chip';
+import DefinitionList from '../../ui/definition-list';
+import TruncatedDefinitionList from '../../ui/truncated-definition-list';
+import Popover from '../../ui/popover';
+import Typography from '../../ui/typography';
+import Box from '../../ui/box';
+import MetaMaskTranslation from '../metamask-translation';
+
+export const safeComponentList = {
+  MetaMaskTranslation,
+  b: 'b',
+  p: 'p',
+  div: 'div',
+  span: 'span',
+  Typography,
+  Chip,
+  DefinitionList,
+  TruncatedDefinitionList,
+  Button,
+  Popover,
+  Box,
+};
